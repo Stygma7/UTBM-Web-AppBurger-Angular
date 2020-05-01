6fb85a080f0bb60a7f9727f412b27c428862cb8e
Matthieu-Hirth <matthieu.hirth.68@gmail.com>
2020-05-01 12:02:26 +0200

    add swagger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiModule } from './API';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
