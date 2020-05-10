23e64757a6d3e3eba08c9df7e1a2a72ffab3decb
Matthieu-Hirth <matthieu.hirth.68@gmail.com>
2020-05-10 19:11:11 +0200

    Add burger-detail + routing

--- src/app/burger-details/burger-details.component.ts
+++ src/app/burger-details/burger-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BurgersService, BurgerDetail } from '../Swagger';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-burger-details',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./burger-details.component.css']
 })
 export class BurgerDetailsComponent implements OnInit {
-
-  constructor() { }
+  burger: BurgerDetail;
+  constructor(
+    private route: ActivatedRoute,
+    private BurgersService: BurgersService,
+    private location: Location
+    ) {}
 
   ngOnInit() {
+    this.getBurger()
   }
 
+  getBurger(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.BurgersService.burgerDetail(id)
+      .subscribe(burger => this.burger = burger);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
